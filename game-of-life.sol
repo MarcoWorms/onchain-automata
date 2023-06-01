6914803b677014115765ddd8daa79b92ddad3f2e
Marco Guaspari Worms <marcogworms@gmail.com>
2023-06-01 13:23:20 -0300

    Update game-of-life.sol

--- game-of-life.sol
+++ game-of-life.sol
@@ -1,71 +1,87 @@
 pragma solidity ^0.8.0;
 
 contract GameOfLife {
-    uint public rows;
-    uint public columns;
+    // State variables to store the grid size and the grid itself
+    uint public x;
+    uint public y;
     bool[][] public grid;
     
-    event GridInitialized(uint rows, uint columns);
-    event CellActivated(uint row, uint column);
+    // Events to be emitted when the grid is initialized, a cell is activated, and an iteration is completed
+    event GridInitialized(uint x, uint y);
+    event CellActivated(uint xCoord, uint yCoord);
     event NextIterationCompleted(bool[][] newGrid);
 
-    constructor(uint _rows, uint _columns) {
-        require(_rows > 0 && _columns > 0, "Row and column sizes must be greater than 0");
-        rows = _rows;
-        columns = _columns;
-        grid = new bool[][](rows);
-        for (uint i = 0; i < rows; i++) {
-            grid[i] = new bool[](columns);
+    // Constructor to initialize the grid with the specified size
+    constructor(uint _x, uint _y) {
+        require(_x > 0 && _y > 0, "X and Y sizes must be greater than 0");
+        x = _x;
+        y = _y;
+        grid = new bool[][](x);
+        for (uint i = 0; i < x; i++) {
+            grid[i] = new bool[](y);
         }
-        emit GridInitialized(rows, columns);
+        emit GridInitialized(x, y);
     }
 
-    function activateCell(uint row, uint column) public {
-        require(row < rows && column < columns, "Invalid coordinates");
-        grid[row][column] = true;
-        emit CellActivated(row, column);
+    // Function to activate a single cell in the grid
+    function activateCell(uint xCoord, uint yCoord) public {
+        require(xCoord < x && yCoord < y, "Invalid coordinates");
+        grid[xCoord][yCoord] = true;
+        emit CellActivated(xCoord, yCoord);
     }
 
-    function activateCells(uint[] memory rowCoords, uint[] memory colCoords) public {
-        require(rowCoords.length == colCoords.length, "Coordinate arrays must have the same length");
+    // Function to activate multiple cells in the grid at once
+    function activateCells(uint[] memory xCoords, uint[] memory yCoords) public {
+        require(xCoords.length == yCoords.length, "Coordinate arrays must have the same length");
 
-        for (uint i = 0; i < rowCoords.length; i++) {
-            activateCell(rowCoords[i], colCoords[i]);
+        for (uint i = 0; i < xCoords.length; i++) {
+            activateCell(xCoords[i], yCoords[i]);
         }
     }
 
+    // Function to compute the next iteration of the grid based on Conway's Game of Life rules
     function nextIteration() public {
-        bool[][] memory newGrid = new bool[][](rows);
-        for (uint i = 0; i < rows; i++) {
-            newGrid[i] = new bool[](columns);
+        // Create a new grid to store the updated state
+        bool[][] memory newGrid = new bool[][](x);
+        for (uint i = 0; i < x; i++) {
+            newGrid[i] = new bool[](y);
         }
 
-        for (uint row = 0; row < rows; row++) {
-            for (uint column = 0; column < columns; column++) {
-                uint8 neighbors = countNeighbors(row, column);
+        // Iterate through each cell in the grid
+        for (uint xCoord = 0; xCoord < x; xCoord++) {
+            for (uint yCoord = 0; yCoord < y; yCoord++) {
+                // Count the number of live neighbors for the current cell
+                uint8 neighbors = countNeighbors(xCoord, yCoord);
 
-                if (grid[row][column]) {
-                    newGrid[row][column] = neighbors == 2 || neighbors == 3;
+                // Apply the rules of Conway's Game of Life to update the new grid
+                if (grid[xCoord][yCoord]) {
+                    newGrid[xCoord][yCoord] = neighbors == 2 || neighbors == 3;
                 } else {
-                    newGrid[row][column] = neighbors == 3;
+                    newGrid[xCoord][yCoord] = neighbors == 3;
                 }
             }
         }
 
+        // Update the grid state with the new grid
         grid = newGrid;
         emit NextIterationCompleted(grid);
     }
 
-    function countNeighbors(uint row, uint column) private view returns (uint8) {
+    // Function to count the number of live neighbors for a given cell
+    function countNeighbors(uint xCoord, uint yCoord) private view returns (uint8) {
         uint8 count = 0;
+        // Iterate through the neighboring cells
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
+                // Skip the current cell itself
                 if (i == 0 && j == 0) continue;
 
-                uint newRow = row + uint(i);
-                uint newColumn = column + uint(j);
+                // Calculate the coordinates of the neighboring cell
+                uint newX = xCoord + uint(i);
+                uint newY = yCoord + uint(j);
 
-                if (newRow < rows && newColumn < columns && grid[newRow][newColumn]) {
+                // Check if the neighboring cell is within the grid boundaries and is alive
+                if (newX < x && newY < y && grid[newX][newY]) {
                     count++;
                 }
             }
