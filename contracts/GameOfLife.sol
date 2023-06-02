0ad427220830905adfc4c1a42bfdbe780ee69bdb
Mario S.O <gh_mario.proclaim@slmail.me>
2023-06-02 13:16:43 -0400

    Simple gas optimization in for loops

--- contracts/GameOfLife.sol
+++ contracts/GameOfLife.sol
@@ -1,51 +1,54 @@
 pragma solidity ^0.8.0;
 
 contract GameOfLife {
-    uint public width;
-    uint public height;
+    uint256 public width;
+    uint256 public height;
     bool[][] public grid;
-    
-    event GridInitialized(uint width, uint height);
-    event CellActivated(uint xCoord, uint yCoord);
+
+    event GridInitialized(uint256 width, uint256 height);
+    event CellActivated(uint256 xCoord, uint256 yCoord);
     event NextIterationCompleted(bool[][] newGrid);
 
-    constructor(uint _width, uint _height) {
+    constructor(uint256 _width, uint256 _height) {
         require(_width > 0 && _height > 0, "Width and height sizes must be greater than 0");
         width = _width;
         height = _height;
         grid = new bool[][](width);
-        for (uint i = 0; i < width; i++) {
+        for (uint256 i = 0; i < width; i++) {
             grid[i] = new bool[](height);
         }
         emit GridInitialized(width, height);
     }
-    
+
     function getGrid() public view returns (bool[][] memory) {
         return grid;
     }
 
-    function activateCell(uint xCoord, uint yCoord) public {
+    function activateCell(uint256 xCoord, uint256 yCoord) public {
         require(xCoord < width && yCoord < height, "Invalid coordinates");
         grid[xCoord][yCoord] = true;
         emit CellActivated(xCoord, yCoord);
     }
 
-    function activateCells(uint[] memory xCoords, uint[] memory yCoords) public {
+    function activateCells(uint256[] memory xCoords, uint256[] memory yCoords) public {
         require(xCoords.length == yCoords.length, "Coordinate arrays must have the same length");
 
-        for (uint i = 0; i < xCoords.length; i++) {
+        for (uint256 i = 0; i < xCoords.length; i++) {
             activateCell(xCoords[i], yCoords[i]);
         }
     }
 
     function nextIteration() public {
         bool[][] memory newGrid = new bool[][](width);
-        for (uint i = 0; i < width; i++) {
+        for (uint256 i = 0; i < width;) {
             newGrid[i] = new bool[](height);
+            unchecked {
+                ++i;
+            }
         }
 
-        for (uint xCoord = 0; xCoord < width; xCoord++) {
-            for (uint yCoord = 0; yCoord < height; yCoord++) {
+        for (uint256 xCoord = 0; xCoord < width;) {
+            for (uint256 yCoord = 0; yCoord < height;) {
                 uint8 neighbors = countNeighbors(xCoord, yCoord);
 
                 if (grid[xCoord][yCoord]) {
@@ -53,6 +56,12 @@ contract GameOfLife {
                 } else {
                     newGrid[xCoord][yCoord] = neighbors == 3;
                 }
+                unchecked {
+                    ++yCoord;
+                }
+            }
+            unchecked {
+                ++xCoord;
             }
         }
 
@@ -60,19 +69,29 @@ contract GameOfLife {
         emit NextIterationCompleted(grid);
     }
 
-    function countNeighbors(uint xCoord, uint yCoord) private view returns (uint8) {
+    function countNeighbors(uint256 xCoord, uint256 yCoord) private view returns (uint8) {
         uint8 count = 0;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
+        for (int256 i = -1; i <= 1;) {
+            for (int256 j = -1; j <= 1;) {
                 // Skip the current cell itself
                 if (i == 0 && j == 0) continue;
 
-                int newX = int(xCoord) + i;
-                int newY = int(yCoord) + j;
+                int256 newX = int256(xCoord) + i;
+                int256 newY = int256(yCoord) + j;
 
-                if (newX >= 0 && newY >= 0 && uint(newX) < width && uint(newY) < height && grid[uint(newX)][uint(newY)]) {
+                if (
+                    newX >= 0 && newY >= 0 && uint256(newX) < width && uint256(newY) < height
+                        && grid[uint256(newX)][uint256(newY)]
+                ) {
                     count++;
                 }
+
+                unchecked {
+                    ++j;
+                }
+            }
+            unchecked {
+                ++i;
             }
         }
 
